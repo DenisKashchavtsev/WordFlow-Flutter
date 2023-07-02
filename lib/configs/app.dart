0ac3d6b2d2a824583e839bba980d97d4c065baa9
Denis Kashchavtsev <dkashavtsev@gmail.com>
2023-07-02 21:00:00 +0300

    adding app config

---
+++ lib/configs/app.dart
@@ -0,0 +1,7 @@
+class App {
+  static const String apiUrl = 'http://localhost:82/';
+  static const Map<String, String> apiHeaders = {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+  };
+}
