480dad8dec24198204a1fdc4dc932408a7425e09
Denis Kashchavtsev <dkashavtsev@gmail.com>
2023-07-18 21:00:00 +0300

    adding auth pages

---
+++ lib/pages/auth/login.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../configs/styles.dart';
+import '../../cubits/user/user_cubit.dart';
+import '../widgets/notifications/error.dart';
+
+class Login extends StatelessWidget {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  Login({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Styles.mainColor,
+        elevation: 0,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              children: <Widget>[
+                Container(
+                  height: 350,
+                  decoration: const BoxDecoration(
+                      color: Styles.mainColor,
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(40.0),
+                        bottomRight: Radius.circular(40.0),
+                      )),
+                ),
+                Container(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 40, horizontal: 45),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text('Login',
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 30)),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text('Please enter your email and password to login',
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 18)),
+                        Text('test@gmail.com:123456',
+                            style: TextStyle(color: Colors.white)),
+                      ],
+                    )),
+                BlocBuilder<UserCubit, UserState>(
+                  builder: (context, state) {
+                    if (state is UserStateLoading) {
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+                    if (state is UserStateError) {
+                      return ErrorNotification(errorMessage: state.message);
+                    }
+                    return const Text('');
+                  },
+                ),
+                Container(
+                  padding: const EdgeInsets.only(top: 240, left: 45, right: 45),
+                  child: Container(
+                    height: 400,
+                    decoration: const BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(Radius.circular(40.0))),
+                    child: Column(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 40, horizontal: 40),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Expanded(
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                    enabledBorder: Styles.inputBorder,
+                                    focusedBorder: Styles.focusBorder,
+                                    hintText: 'Email',
+                                  ),
+                                  controller: _emailController,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.only(
+                              top: 0, bottom: 40, left: 40, right: 40),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Expanded(
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                    enabledBorder: Styles.inputBorder,
+                                    focusedBorder: Styles.focusBorder,
+                                    hintText: 'Password',
+                                  ),
+                                  controller: _passwordController,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.only(
+                              top: 0, bottom: 40, left: 40, right: 40),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Expanded(
+                                child: ElevatedButton(
+                                    onPressed: () {
+                                      context.read<UserCubit>().login(
+                                          _emailController.text,
+                                          _passwordController.text);
+                                    },
+                                    style: Styles.buttonFilledStyles,
+                                    child: const Text('Login')),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.only(
+                              top: 0, bottom: 40, left: 40, right: 40),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Expanded(
+                                child: TextButton(
+                                  onPressed: () {
+                                    Navigator.pushNamed(context, '/register');
+                                  },
+                                  child: const Text(
+                                    'Register',
+                                    style: TextStyle(
+                                        color: Styles.mainColor, fontSize: 18),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/auth/register.dart
@@ -0,0 +1,183 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../configs/styles.dart';
+import '../../cubits/user/user_cubit.dart';
+import '../widgets/notifications/error.dart';
+
+class Register extends StatelessWidget {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  Register({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Styles.mainColor,
+        elevation: 0,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              children: <Widget>[
+                Container(
+                  height: 350,
+                  decoration: const BoxDecoration(
+                      color: Styles.mainColor,
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(40.0),
+                        bottomRight: Radius.circular(40.0),
+                      )),
+                ),
+                Container(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 40, horizontal: 45),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text('Register',
+                            style: TextStyle(color: Colors.white, fontSize: 30)),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text('Please enter your email and password to register',
+                            style: TextStyle(color: Colors.white, fontSize: 18)),
+                      ],
+                    )),
+                BlocBuilder<UserCubit, UserState>(
+                  builder: (context, state) {
+                    if (state is UserStateLoading) {
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+                    if (state is UserStateError) {
+                      return ErrorNotification(errorMessage: state.message);
+                    }
+                    return const Text('');
+                  },
+                ),
+                Container(
+                  padding: const EdgeInsets.only(top: 240, left: 45, right: 45),
+                  child: Container(
+                    height: 490,
+                    decoration: const BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(Radius.circular(40.0))),
+                    child: Column(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 40, horizontal: 40),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Expanded(
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                    enabledBorder: Styles.inputBorder,
+                                    focusedBorder: Styles.focusBorder,
+                                    hintText: 'Name',
+                                  ),
+                                  controller: _nameController,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.only(
+                              top: 0, bottom: 40, left: 40, right: 40),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Expanded(
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                    enabledBorder: Styles.inputBorder,
+                                    focusedBorder: Styles.focusBorder,
+                                    hintText: 'Email',
+                                  ),
+                                  controller: _emailController,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.only(
+                              top: 0, bottom: 40, left: 40, right: 40),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Expanded(
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                    enabledBorder: Styles.inputBorder,
+                                    focusedBorder: Styles.focusBorder,
+                                    hintText: 'Password',
+                                  ),
+                                  controller: _passwordController,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.only(
+                              top: 0, bottom: 40, left: 40, right: 40),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Expanded(
+                                child: ElevatedButton(
+                                    onPressed: () {
+                                      context.read<UserCubit>().register(
+                                          context,
+                                          _nameController.text,
+                                          _emailController.text,
+                                          _passwordController.text);
+                                    },
+                                    style: Styles.buttonFilledStyles,
+                                    child: const Text('Register')),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.only(
+                              top: 0, bottom: 40, left: 40, right: 40),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Expanded(
+                                child: TextButton(
+                                  onPressed: () {
+                                    Navigator.pushNamed(context, '/login');
+                                  },
+                                  child: const Text(
+                                    'Login',
+                                    style: TextStyle(
+                                        color: Styles.mainColor, fontSize: 18),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
