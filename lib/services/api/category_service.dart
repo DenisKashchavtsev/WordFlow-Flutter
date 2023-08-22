b3114e5bf93dea66dc3a8760aaced0f807a468df
Denis Kashchavtsev <dkashavtsev@gmail.com>
2023-08-22 21:00:00 +0300

    adding category service

---
+++ lib/services/api/category_service.dart
@@ -0,0 +1,107 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+
+import '../../http/dio_client.dart';
+import '../../models/word_category.dart';
+import '../../models/word_word.dart';
+
+class CategoryService {
+  Dio get _dio => DioClient().getClient();
+
+  Future<List<WordCategory>> getList(int page) async {
+    try {
+      final response = await _dio.get('/api/word-categories?page=$page');
+
+      if (jsonDecode(response.data)['data'] is List) {
+        List<WordCategory> categoryList =
+            (jsonDecode(response.data)['data'] as List).map((item) {
+          return WordCategory(
+            id: item['id'],
+            name: item['name'],
+            image: item['image'],
+            public: item['public'],
+          );
+        }).toList();
+
+        return categoryList;
+      } else {
+        return [];
+      }
+    } catch (e) {
+      throw Exception('Error getting data: $e');
+    }
+  }
+
+  Future<Map<String, dynamic>> create(String name) async {
+    try {
+      final response = await _dio.post('/api/word-categories', data: {
+        'name': name,
+      });
+
+      return response.data;
+    } catch (e) {
+      throw Exception('Error: $e');
+    }
+  }
+
+  Future<Map<String, dynamic>> update(String id, String name) async {
+    try {
+      final response = await _dio.put('/api/word-categories/$id', data: {
+        'name': name,
+      });
+
+      return response.data;
+    } catch (e) {
+      throw Exception('Error: $e');
+    }
+  }
+
+  Future<WordCategory> show(String id) async {
+    try {
+      final response = await _dio.get('/api/word-categories/$id');
+
+      return WordCategory(
+        id: response.data['id'],
+        name: response.data['name'],
+        image: response.data['image'] ?? '',
+        public: response.data['public'] ?? false,
+      );
+    } catch (e) {
+      throw Exception('Error getting data: $e');
+    }
+  }
+
+  Future<List<WordWord>> getCategoryWords(String id, int page) async {
+    try {
+      final response = await _dio.get('/api/word-categories/$id/words');
+      final data = jsonDecode(response.data)['data'];
+      if (data is List) {
+        List<WordWord> wordList = (data).map((item) {
+          return WordWord(
+            id: item['id'],
+            translate: item['translate'],
+            source: item['source'],
+          );
+        }).toList();
+
+        print(wordList);
+        return wordList;
+      } else {
+        return [];
+      }
+    } catch (e) {
+      throw Exception('Error getting data: $e');
+    }
+  }
+
+  Future delete(List ids) async {
+    try {
+      await _dio.delete('/api/word-categories', data: {
+        'ids': ids
+      });
+    } catch (e) {
+      throw Exception('Error: $e');
+    }
+  }
+}
